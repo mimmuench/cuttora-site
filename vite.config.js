4f78edc075676e2a904a5ffdaef9c88086677841
Silkem <silkemuenchx@gmail.com>
2025-12-06 23:33:49 +0300

    build: output to docs for github pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",   // BUNU EKLEDİK – GitHub Pages için
+  base: "/",                 // domain kökü
+  build: {
+    outDir: "docs",          // dist yerine docs klasörüne build al
+  },
 })
