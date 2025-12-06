0c23ae53652af6243a3f6ebf25006aecca76b3da
Silkem <silkemuenchx@gmail.com>
2025-12-06 23:21:30 +0300

    initial deploy

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",   // <<< BUNU EKLEDİK — GITHUB PAGES İÇİN ŞART
+  base: "/",   // BUNU EKLEDİK – GitHub Pages için
 })
