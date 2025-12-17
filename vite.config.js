23d91793c256d83e93b9a73fff99e067c07a56d1
Silkem <silkemuenchx@gmail.com>
2025-12-17 18:09:59 +0300

    Fix base path for custom domain

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/cuttora/', // Repo adınızı buraya yazın
+  base: '/',  // ← Burası önemli! Custom domain için / olmalı
   build: {
     outDir: 'docs'
   }
